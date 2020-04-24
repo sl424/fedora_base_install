3a4cf813ff29db8e8dbb1750da577693e8631324
sl424 <sl424@parker.Home>
2020-04-23 23:05:34 -0700

    update fedora minimal install

--- fedora.sh
+++ fedora.sh
@@ -5,32 +5,41 @@ TIMEZONE="/usr/share/zoneinfo/America/Los_Angeles"
 HOSTNAME="fedoralinux"
 USER_NAME="sl424"
 
-EFI_PART="/dev/sda1"
-ESP_DIRECTORY = "/boot/efi"
+#EFI_PART="/dev/sda1"
+#ROOT_PART="/dev/sda5"
+#ROOT_UUID=$(lsblk $ROOT_PART -o UUID | tail -n 1)
 
-ROOT_PART="/dev/sda5"
-ROOT_UUID=$(lsblk $ROOT_PART -o UUID | tail -n 1)
+ROOT_PARTLABEL="FEDORA"
+ESP_DIRECTORY="/boot/efi"
 
 
-pkgs="iwl7260-firmware NetworkManager-wifi \ 
-sway swaylock swayidle waybar mesa-dri-drivers wl-clipboard  \
-brightlight alsa-utils \
-git firefox vim  \
-intel-media-driver libva-utils \
-xorg-x11-server-utils\
-unzip wget \
+pkgs="iwl7260-firmware NetworkManager-wifi  
+sway swaylock swayidle waybar mesa-dri-drivers wl-clipboard  
+brightlight alsa-utils 
+git vim  
+xorg-x11-server-utils 
+unzip wget 
+terminus-fonts 
 "
+#intel-media-driver libva-utils 
 
 function cpdotfile(){
 	giturl="https://github.com/sl424/dotfile.git"
 	homedir=/home/$USER_NAME
         bash -c "su $USER_NAME -c \" git clone --separate-git-dir=$homedir/.dotfiles $giturl $homedir/tmp \" "
-        bash -c "su $USER_NAME -c \" cd $homedir/tmp; for file in .*; do if [ -f $file ]; then cp -r $file $homedir/$file; fi; done \" "
+        bash -c "su $USER_NAME -c \" cd $homedir/tmp; for file in .*; do if [ -f $file ]; then cp -r $file $homedir; fi; done \" "
+        bash -c "su $USER_NAME -c \" konfig checkout t450s \" "
 }
 
 lowbat_dir="/etc/udev/rules.d/99-lowbat.rules"
-lowbat="# Suspend the system when battery level drops to 5% or lower \n \
-SUBSYSTEM==\"power_supply\", ATTR{status}==\"Discharging\", ATTR{capacity}==\"[0-5]\", RUN+=\"/usr/bin/systemctl suspend\"   \n \
+lowbat="# Suspend the system when battery level drops to 9% or lower \n \
+SUBSYSTEM==\"power_supply\", ATTR{status}==\"Discharging\", ATTR{capacity}==\"[0-9]\", RUN+=\"/usr/bin/systemctl suspend\"   \n \
+SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto" \n\
+SUBSYSTEM=="pci", ATTR{power/control}="auto" \n\
+## Remove NVIDIA devices, if present \n\
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1" \n\
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1" \n\
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1" \n\
 "
 
 thinkpad_dir="/etc/modprobe.d/thinkpad.conf"
@@ -76,11 +85,14 @@ function systemd() {
     echo "title Fedora Linux" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
 
 	vm=$(cd /boot; ls vmlinuz-*86*)
-    echo "efi /F/vmlinuz-linux" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
-	initram=$(cd /boot; ls initramfs-*86*)
-    echo "initrd /F/initramfs-linux.img" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
+	cp /boot/$vm $ESP_DIRECTORY/F/vmlinuz
+    echo "efi /F/vmlinuz" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
 
-    echo "options root=UUID=$ROOT_UUID rw" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
+	initram=$(cd /boot; ls initramfs-*86*)
+	cp /boot/$initram $ESP_DIRECTORY/F/initramfs.img
+    echo "initrd /F/initramfs.img" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
+
+    echo "options root=PARTLABEL=$ROOT_PARTLABEL rw" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
     echo "options rhgb quiet" >> "$ESP_DIRECTORY/loader/entries/fedora.conf"
 }
 
@@ -91,13 +103,12 @@ function packages() {
 }
 
 function add_repo(){
-	dnf copr enable alebastr/waybar 
 	dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 function dnf_install() {
     PACKAGES=$1
-    for VARIABLE in {1..5}
+    for TRY in {1..5}
     do
         dnf --assumeyes install $PACKAGES
         if [ $? == 0 ]; then
@@ -144,4 +155,9 @@ function main() {
     cpdotfile
 }
 
-main
+#main
+if [ ! -z $1 ]; then
+	$1
+else
+	echo " fedora.sh [ chroot | add_repo | packages | hwmod | configure | cpdotfile | systemd ]"
+fi
